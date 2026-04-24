8937924ab9c8af4ae28892cddaf7cc5784f921f3
smartform591-arch <smartform591@gmail.com>
2026-04-24 09:10:18 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'smartfuh-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/inicio.html',
+  '/login.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Estrategia: Primero caché, luego red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
